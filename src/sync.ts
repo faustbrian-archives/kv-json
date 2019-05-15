dbfedb319764a819dfe5eac4334a7e6965d71c62
Brian Faust <hello@basecode.sh>
2019-05-15 06:11:57 +0300

    @keeveestore/file:0.1.2

--- src/sync.ts
+++ src/sync.ts
@@ -1,31 +1,21 @@
 // tslint:disable: no-unsafe-any
 import { StoreSync as AbstractStore } from "@keeveestore/file";
-import { ensureFileSync, readJSONSync, writeJSONSync } from "fs-extra";
+import { readJSONSync, writeJSONSync } from "fs-extra";
 
 export class StoreSync<K, T> extends AbstractStore<K, T> {
 	public static new<K, T>(uri: string): StoreSync<K, T> {
 		return new StoreSync<K, T>(new Map<K, T>(), uri);
 	}
 
-	protected dump(): void {
-		// @ts-ignore
-		const rows: Record<K, T> = {};
-
-		for (const [key, value] of this.all()) {
-			rows[key] = value;
-		}
-
+	// @ts-ignore
+	protected dump(rows: Record<K, T>): void {
 		writeJSONSync(this.uri, rows);
 	}
 
 	protected load(): void {
-		ensureFileSync(this.uri);
-
-		try {
-			for (const [key, value] of Object.entries(readJSONSync(this.uri))) {
-				// @ts-ignore
-				this.put(key, value);
-			}
-		} catch (error) {} // tslint:disable-line: no-empty
+		for (const [key, value] of Object.entries(readJSONSync(this.uri))) {
+			// @ts-ignore
+			this.put(key, value);
+		}
 	}
 }
